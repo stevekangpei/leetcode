3845f000b57cf8dd9e2e0256d10cf9ba5631f863
stevekangpei <stevekangp@outlook.com>
2020-03-02 10:12:50 +0800

    second commit

---
+++ src/com/kangpei/leetcode/listnode/ListNode.java
@@ -0,0 +1,12 @@
+package com.kangpei.leetcode.listnode;
+
+/**
+ * Created by kangpei on 2020/3/2.
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+}
---
+++ src/com/kangpei/leetcode/listnode/easy/MergeTwoSortedListNode.java
@@ -0,0 +1,7 @@
+package com.kangpei.leetcode.listnode.easy;
+
+/**
+ * Created by kangpei on 2020/3/2.
+ */
+public class MergeTwoSortedListNode {
+}
